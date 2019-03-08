2b15103f216fbb901e8553315ddd8df74440fbeb
Shivaar Sooklal <shivaarsooklal.108@gmail.com>
2019-03-08 18:57:15 +0000

    Upload version.php file

---
+++ BookKing/version.php
@@ -0,0 +1,9 @@
+<?php
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'BookKing';
+$plugin->version = 2015122100;
+$plugin->requires = 2015122100;
+$plugin->maturity = MATURITY_STABLE;
+$plugin->release = 'v3.0.1-r1'
