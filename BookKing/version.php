ecd22ee84ae13960f3dc8f0daed7e53bffc7dcb4
Hashmeel Suka <1579927@students.wits.ac.za>
2019-03-11 12:47:54 +0000

    Upload root version.php File

---
+++ BookKing/version.php
@@ -0,0 +1,9 @@
+<?php
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version   = 2019031000;
+$plugin->requires  = 2015042800;
+$plugin->component = 'mod_bookking'; // Full name of the plugin (used for diagnostics).
+$plugin->maturity  = MATURITY_STABLE;
+$plugin->release   = '0.0.1';
