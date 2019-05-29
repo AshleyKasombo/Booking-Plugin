494d5d875ff8958c87c7212145fc2f40ee925db8
Siddeeq Laher <siddeeqlaher@gmail.com>
2019-05-29 20:28:37 +0000

    Upload New File

---
+++ BookKing/classes/search/activity.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Search area for BookKing activities.
+ *
+ */
+
+namespace mod_bookking\search;
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Search area for mod_bookking activities.
+ *
+ */
+class activity extends \core_search\base_activity {
+}
