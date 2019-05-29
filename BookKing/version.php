806d63c695baed18578dc8ce2c0eb96acb5c57bd
Hashmeel Suka <1579927@students.wits.ac.za>
2019-05-29 20:41:48 +0000

    Upload New File

---
+++ BookKing/version.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Version information for mod/bookking
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+/*
+ * This is the development branch (master) of the bookking module.
+ */
+
+$plugin->component = 'mod_bookking'; // Full name of the plugin (used for diagnostics).
+$plugin->version   = 2018112600;      // The current module version (Date: YYYYMMDDXX).
+$plugin->release   = '3.5.0';         // Human-friendly version name.
+$plugin->requires  = 2018051700;      // Requires Moodle 3.5.
+$plugin->maturity  = MATURITY_STABLE; // Stable release
